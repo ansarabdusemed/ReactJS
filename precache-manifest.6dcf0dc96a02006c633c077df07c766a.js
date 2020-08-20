8a1c731c4e3dc481f85f80d666660656caaa18ce
Ansar Bahrun <ansarbahrun@Ansars-MacBook-Air.local>
2020-08-19 23:15:09 -0700

    Updates

---
+++ precache-manifest.6dcf0dc96a02006c633c077df07c766a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d043184a0b4e382e8a74e14b82f56de",
+    "url": "/ReactJS/index.html"
+  },
+  {
+    "revision": "22dfe8b87f54df8a24ff",
+    "url": "/ReactJS/static/css/main.e0a9b624.chunk.css"
+  },
+  {
+    "revision": "fe0580be2888b2de49c0",
+    "url": "/ReactJS/static/js/2.ec8f0743.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactJS/static/js/2.ec8f0743.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22dfe8b87f54df8a24ff",
+    "url": "/ReactJS/static/js/main.2b06957f.chunk.js"
+  },
+  {
+    "revision": "b69144fcfddb74ef15b6",
+    "url": "/ReactJS/static/js/runtime-main.c645ba9e.js"
+  },
+  {
+    "revision": "96e721c65a5206a9653b6f2bf6eef132",
+    "url": "/ReactJS/static/media/ABTLogo.96e721c6.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ReactJS/static/media/logo.5d5d9eef.svg"
+  }
+]);
